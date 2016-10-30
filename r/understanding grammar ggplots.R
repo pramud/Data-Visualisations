42b2f08bb4edbe4f1cac77b89acb1593dca9ed82
Pramud Bommakanti <naga92@gmail.com>
2016-10-30 22:02:25 +0530

    Added one more example

--- r/understanding grammar ggplots.R
+++ r/understanding grammar ggplots.R
@@ -12,3 +12,21 @@ dia_plot + geom_point()
 # Add the same geom layer, but with aes() inside
 
 dia_plot + geom_point(aes(col = clarity))
+
+
+# The dia_plot object has been created for you. Update dia_plot so that this time it contains all the functions to make a scatter plot instead of just the data and aes layers. Your command should start with dia_plot <- dia_plot + .... For this instruction, use geom_point() for the geom layer. Set alpha to 0.2. Using +, plot the dia_plot object with a geom_smooth() layer on top. You don't want any error shading, which can be achieved by setting the se argument in geom_smooth() to FALSE. In order to plot the figure, your command should start with dia_plot + ... here. Modify the geom_smooth() function from the previous instruction so that it contains aes() and map clarity to the col argument.
+
+set.seed(1)
+
+# The dia_plot object has been created for you
+dia_plot <- ggplot(diamonds, aes(x = carat, y = price))
+
+# Expand dia_plot by adding geom_point() with alpha set to 0.2
+dia_plot <- dia_plot + geom_point(alpha = 0.2)
+
+
+# Plot dia_plot with additional geom_smooth() with se set to FALSE
+dia_plot <- dia_plot + geom_smooth(se = F)
+
+# Copy the command from above and add aes() with the correct mapping to geom_smooth()
+dia_plot <- dia_plot + geom_smooth(se = F, aes(col=clarity))
