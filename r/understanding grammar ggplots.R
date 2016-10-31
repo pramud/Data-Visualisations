4aeaad87212972fcb383733b63696097d35ffdb7
Pramud Bommakanti <naga92@gmail.com>
2016-10-31 10:29:38 +0530

    Update understanding grammar ggplots.R

--- r/understanding grammar ggplots.R
+++ r/understanding grammar ggplots.R
@@ -1,10 +1,14 @@
-#Here we explore some of the different grammatical elements. Throughout this course we'll discover how they can be combined in all sorts of ways to develop unique plots. In the following instructions, you'll start by creating a ggplot object from the diamonds dataset. Next, you'll add layers onto this object to build beautiful plots.
-
-#Define the data and aesthetics layers: map carat on the x axis and price on the y axis. Assign it to an object: dia_plot. Using +, add a layer, geom_point(), to the dia_plot object. This can be in a single or multiple lines. We can also call aes() within the geom_point() function. Try mapping clarity to the col argument in this way.
+# Exploring some of the different grammatical elements. 
+# Discovering how they can be combined in all sorts of ways to develop unique plots.
+# start by creating a ggplot object from the diamonds dataset. 
+# Next,  add layers onto this object to build beautiful plots.
+# Define the data and aesthetics layers: map carat on the x axis and price on the y axis. 
+# Assign it to an object: dia_plot. Using +, add a layer, geom_point(), to the dia_plot object. 
+# This can be in a single or multiple lines. We can also call aes() within the geom_point() function. 
+# Try mapping clarity to the col argument in this way.
 
 # Create the object containing the data and aes layers: dia_plot
 dia_plot <- ggplot(diamonds,aes(x=carat,y=price))
-
 
 # Add a geom layer with + and geom_point()
 dia_plot + geom_point()
@@ -13,17 +17,19 @@ dia_plot + geom_point()
 
 dia_plot + geom_point(aes(col = clarity))
 
-
-# The dia_plot object has been created for you. Update dia_plot so that this time it contains all the functions to make a scatter plot instead of just the data and aes layers. Your command should start with dia_plot <- dia_plot + .... For this instruction, use geom_point() for the geom layer. Set alpha to 0.2. Using +, plot the dia_plot object with a geom_smooth() layer on top. You don't want any error shading, which can be achieved by setting the se argument in geom_smooth() to FALSE. In order to plot the figure, your command should start with dia_plot + ... here. Modify the geom_smooth() function from the previous instruction so that it contains aes() and map clarity to the col argument.
+# Update dia_plot so that this time it contains all the functions to make a scatter plot instead of just the data and aes layers. 
+# dia_plot <- dia_plot + .... 
+# Set alpha to 0.2. Using +, plot the dia_plot object with a geom_smooth() layer on top. 
+# You don't want any error shading, which can be achieved by setting the se argument in geom_smooth() to FALSE. 
+# In order to plot the figure, your command should start with dia_plot + ... here. 
+# Modify the geom_smooth() function from the previous instruction so that it contains aes() and map clarity to the col argument.
 
 set.seed(1)
 
-# The dia_plot object has been created for you
 dia_plot <- ggplot(diamonds, aes(x = carat, y = price))
 
 # Expand dia_plot by adding geom_point() with alpha set to 0.2
 dia_plot <- dia_plot + geom_point(alpha = 0.2)
-
 
 # Plot dia_plot with additional geom_smooth() with se set to FALSE
 dia_plot <- dia_plot + geom_smooth(se = F)
